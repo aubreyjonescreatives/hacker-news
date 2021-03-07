cb90b062f1176a10bcd436b23ff197cae0f3cd80
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-03-07 00:51:36 -0700

    Added ReadMe info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react'; 
+import React, { Component } from 'react'; 
 import './App.css';
 import './css/hackerStyles.css'
-import axios from 'axios'
 import Card from '@material-ui/core/Card'; 
 import CardActionArea from '@material-ui/core/CardActionArea'; 
 import CardActions from '@material-ui/core/CardActions'; 
@@ -14,99 +13,83 @@ import Typography from '@material-ui/core/Typography';
 
 
 
-const App = () => {
+class App extends Component {
+constructor() {
+  super(); 
+  this.state = { data: []}; 
+}
 
-/*
-const [hackerData, setHackerData] = useState({
-  hackerList: []
-})
+async componentDidMount() {
+  fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
+  .then(res => res.json())
+  .then(json => this.setState({ data: json}))
+  .catch('Failed to Connect')
+}
 
 
-const fetchHackerNews = () => {
-  axios({
-    method: 'get', 
-    url: `https://hacker-news.firebaseio.com/v0/topstories.json?`, 
-    params: {'print': 'pretty'}
+
+
+render(){
+return (
+
+  <div>
+  {this.state.data.map((hackerItem) => {
+  
+  return (
+    <Card>
+    <CardActionArea>
+      <CardMedia
+        title="Contemplative Reptile"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+         {hackerItem}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="h2">
+         {hackerItem.title}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="h2">
+         {hackerItem.score}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="h2">
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Button size="small" color="primary">
+      {hackerItem.url}
+      </Button>
+    </CardActions>
+  </Card>
+  
+  
+  
+  )
+  
+  
   })
-
-
-.then(function (response) {
-    setHackerData({
-      hackerList: response.data
-    })
-})
-}
-
-useEffect(() => {
-  fetchHackerNews()
-})
-
-*/
-
-async function getHackerData() {
-  try {
-    const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
-    return await response.json()
-  } catch (error) {
-    console.error(error)
+  
   }
-}
+  
+  
+  
+  </div>
+  
+  
+  
+  )}
 
-let hackerData = {} 
-getHackerData().then(data => hackerData = data)
-
-
-return (
-
-<div>
-{hackerData.data.map((hackerItem) => {
-
-return (
-  <Card>
-  <CardActionArea>
-    <CardMedia
-      title="Contemplative Reptile"
-    />
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="h2">
-       {hackerItem}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h2">
-       {hackerItem.title}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h2">
-       {hackerItem.score}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h2">
-      </Typography>
-      <Typography variant="body2" color="textSecondary" component="p">
-        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-        across all continents except Antarctica
-      </Typography>
-    </CardContent>
-  </CardActionArea>
-  <CardActions>
-    <Button size="small" color="primary">
-    {hackerItem.url}
-    </Button>
-  </CardActions>
-</Card>
-
-
-
-)
-
-
-})
 
 }
 
 
 
-</div>
 
 
-
-)}
 
 export default App;
+
